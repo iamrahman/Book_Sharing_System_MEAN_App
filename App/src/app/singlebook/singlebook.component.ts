a430deffe78d11ec38436424ce34a3b78bd5f807
iamrahman <inamur.rahman41@gmail.com>
2019-04-16 14:24:46 +0530

    Few Changes

--- App/src/app/singlebook/singlebook.component.ts
+++ App/src/app/singlebook/singlebook.component.ts
@@ -19,8 +19,9 @@ export class SinglebookComponent implements OnInit {
 
   lat: number;
   lng: number;
-  
-
+  distance: any;
+  geolocation:any;
+  maxdistance:number;
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -43,11 +44,27 @@ export class SinglebookComponent implements OnInit {
         err => {
           console.log(err);
         }
-      ); 
+      );
+  //Geo Location
+      if (navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition((pos)=>this.showPosition(pos));
+      } else { 
+        console.log("Geolocation is not supported by this browser.");
+      }
   }
+  //End Og ngOnInit
 
-  sendEmailToOwner(){
-    alert("Hello");
+  showPosition(position) {
+    this.geolocation = position.coords;
+    console.log(this.geolocation+ this.lng);
+
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((this.lat - this.geolocation.latitude) * p)/2 + 
+          c(this.geolocation.latitude * p) * c(this.lat * p) * 
+          (1 - c((this.lng - this.geolocation.longitude) * p))/2;
+    this.distance = Number((12742 * Math.asin(Math.sqrt(a))).toFixed(1));
+    this.maxdistance = Number(this.distance + 1);
   }
 
 
